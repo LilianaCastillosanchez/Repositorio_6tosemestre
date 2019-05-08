30bcbf2461a511b384c61b0789caa3507c2cd3d1
Liliana <Lilianacastillo045@gmail.com>
2019-05-08 10:04:09 -0500

    Cambio fondo

--- Menu catalogo/Menu catalogo/ventas.Designer.vb
+++ Menu catalogo/Menu catalogo/ventas.Designer.vb
@@ -50,7 +50,7 @@ Partial Class ventas
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(498, 329)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "ventas"
